a60633c25676de48330ecd75c11938e36326eb49
Taiwo-John007 <tayegiggs@gmail.com>
2025-08-18 22:20:47 +0100

    modified ami and private key

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-0de716d6197524dd9"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
